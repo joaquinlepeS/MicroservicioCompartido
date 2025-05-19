1975752483de0303ed061be2f86dfe7889aa8dba
joaquinlepeS <joa.lepe@duocuc.cl>
2025-05-19 10:40:26 -0400

    Inicio proyecto

---
+++ src/main/java/MicroservicioCompartido/Controller/PagoController.java
@@ -0,0 +1,18 @@
+package MicroservicioCompartido.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pago")
+public class PagoController {
+
+    @GetMapping
+    @GetMapping("/{id}")
+    @GetMapping
+    @PostMapping
+    @
+
+}
