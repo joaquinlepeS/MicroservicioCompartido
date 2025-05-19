d6e0e73d83fa94a0cda35b4e3dffed1493ff4c5b
joaquinlepeS <joa.lepe@duocuc.cl>
2025-05-19 10:11:47 -0400

    Inicio proyecto

---
+++ src/main/java/MicroservicioCompartido/MicroservicioCompartido/MicroservicioCompartidoApplication.java
@@ -0,0 +1,13 @@
+package MicroservicioCompartido.MicroservicioCompartido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCompartidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCompartidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MicroservicioCompartido/MicroservicioCompartido/MicroservicioCompartidoApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroservicioCompartido.MicroservicioCompartido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCompartidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
